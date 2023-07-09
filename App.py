b390d80750090c5b3360482a0901ecb80f5c24a0
Saqif-Ayaan <135543577+Saqif-Ayaan@users.noreply.github.com>
2023-07-09 06:24:41 -0700

    done with home and classifier page

--- App.py
+++ App.py
@@ -5,8 +5,12 @@ def initApp():
     app.config['SECRET_KEY'] = 'password'
 
     from Link import start
+    from Link import detector
+    from Link import info
 
     app.register_blueprint(start, url_prefix = '/')
+    app.register_blueprint(detector, url_prefix = '/')
+    app.register_blueprint(info, url_prefix = '/')
 
     return app
     
--- Link.py
+++ Link.py
@@ -1,7 +1,17 @@
 from flask import render_template, Blueprint, request
 
 start = Blueprint('start', __name__)
+detector = Blueprint('detector', __name__)
+info = Blueprint('info', __name__)
 
 @start.route('/')
 def home():
-    return render_template("HomePage.html")
+    return render_template("home.html")
+
+@detector.route('/detector')
+def detect():
+    return render_template("detector.html")
+
+@info.route('/info')
+def inf():
+    return render_template("info.html")
