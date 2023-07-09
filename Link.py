b0e26d9f6ac58b2c55b2013f50aea0d21fff82c7
Kazu Pang <72171546+Kazu-Pang@users.noreply.github.com>
2023-07-09 08:04:02 -0700

    final

--- Link.py
+++ Link.py
@@ -1,4 +1,7 @@
 from flask import render_template, Blueprint, request
+import os
+from PIL import Image
+import roboflow
 
 start = Blueprint('start', __name__)
 detector = Blueprint('detector', __name__)
@@ -8,8 +11,21 @@ info = Blueprint('info', __name__)
 def home():
     return render_template("home.html")
 
-@detector.route('/detector')
+@detector.route('/detector', methods = ["POST", "GET"])
 def detect():
+    if request.method == "POST":
+        file = request.files['Insert_photo']
+        file.save(os.path.join(file.filename))
+
+        rf = roboflow.Roboflow(api_key = "7gfJQ83G4t1jbyfIhIPM")
+        project = rf.project("garbageclassification-bhacz")
+        model = project.version("1").model
+
+        prediction = model.predict(file.filename)
+        pval = prediction.predictions
+        print(pval)
+
+        return render_template("detector.html", pred = pval)
     return render_template("detector.html")
 
 @info.route('/info')
--- Main.py
+++ Main.py
@@ -3,4 +3,4 @@ from App import initApp
 app = initApp()
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(port = 8000, debug = True)
